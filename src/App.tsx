c6242746e9f9612d09fe3a6952b9bb4de002a163
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-10-13 23:31:59 -0400

    Wire up data load and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
 import * as React from 'react'
 
-class App extends React.Component {
+interface State {
+  isDataLoaded: boolean
+}
+
+interface GeodataSource {
+  name: string
+  description: string
+  url: string
+  queryable: boolean
+  downloadable: boolean
+  mapped: boolean
+  spatialExtent: string
+  temporalExtent: string
+  subjects: string[]
+  keywords: string[]
+}
+
+interface ApplicationData {
+  subjects: string[]
+  sources: GeodataSource[]
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    isDataLoaded: false
+  }
+
+  private data: ApplicationData
+
+  async componentDidMount() {
+    await this.fetchData()
+    this.setState({
+      isDataLoaded: true
+    })
+  }
+
+  fetchData = async () => {
+    const response = await fetch('/data.json')
+    const data = await response.json()
+    this.data = data as ApplicationData
+  }
+
   public render() {
-    return <div>hi</div>
+    if (this.state.isDataLoaded) {
+      return (
+        <div>
+          {JSON.stringify(this.data.sources.map(s => s.name))}
+        </div>
+      )
+    } else {
+      return <Loading />
+    }
   }
 }
+
+const Loading = () => <div>Loading…</div>
 
 export default App
